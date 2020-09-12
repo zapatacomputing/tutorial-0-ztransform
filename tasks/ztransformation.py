c8af177afded1c96f5993115589f636e5a40e2fe
Ethan Hansen <1ethanhansen@protonmail.com>
2020-09-11 19:22:05 -0700

    wow i'm so dumb
    literally just forgot a semicolon

--- tasks/ztransformation.py
+++ tasks/ztransformation.py
@@ -1,7 +1,7 @@
 import json
 from orquestra import ztransform
 
-def z_transformation(message)
+def z_transformation(message):
     with open(message, 'r') as f:
         message = json.load(f)
     ztransform(message)
